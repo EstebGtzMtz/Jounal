096024aa20e67dcc89eb920f8fd1cc908eb7ebfa
EstebGtzMtz <esteb.gtz.mtz@hotmail.com>
2023-08-29 13:09:53 -0600

    add layout to auth pages

--- src/auth/Pages/RegisterPage.jsx
+++ src/auth/Pages/RegisterPage.jsx
@@ -1,5 +1,54 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../Layout/AuthLayout';
+
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title='Register'>
+      <form>
+        <Grid container>
+        <Grid item xs={12} sx={{marginTop: 2}}>
+            <TextField
+              label='Full name'
+              type='text'
+              placeholder="John Doe"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{marginTop: 2}}>
+            <TextField
+              label='Email'
+              type='email'
+              placeholder="correro@gmail.com"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{marginTop: 2}}>
+            <TextField
+              label='Password'
+              type='password'
+              placeholder="password"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
+            <Grid item xs={12}>
+              <Button variant='contained' fullWidth>
+                Create account
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction='row' justifyContent='end'>
+            <Typography mr={1} display='inline'>Already have an account?</Typography>
+            <Link component={RouterLink} to='/auth/login'>
+              Login
+            </Link>
+          </Grid>
+
+        </Grid>
+      </form>
+    </AuthLayout>
   )
 }
