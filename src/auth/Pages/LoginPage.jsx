0c58e1da60ca61f9afb6c77ee95d2cbf5cb338b5
EstebGtzMtz <esteb.gtz.mtz@hotmail.com>
2023-09-04 20:56:51 -0600

    add useForm custom hook to conect into the login page

--- src/auth/Pages/LoginPage.jsx
+++ src/auth/Pages/LoginPage.jsx
@@ -2,11 +2,24 @@ import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Google } from '@mui/icons-material';
 import { AuthLayout } from '../Layout/AuthLayout';
+import { useForm } from '../../hooks';
 
 export const LoginPage = () => {
+
+  const { email, password, onInputChange, onResetForm } = useForm({
+    email: 'hola@mail.com',
+    password:'qwerty'
+  })
+
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log({email, password})
+  }
+
   return (
     <AuthLayout title='Login'>
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{marginTop: 2}}>
             <TextField
@@ -14,6 +27,9 @@ export const LoginPage = () => {
               type='email'
               placeholder="correro@gmail.com"
               fullWidth
+              name='email'
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{marginTop: 2}}>
@@ -22,12 +38,15 @@ export const LoginPage = () => {
               type='password'
               placeholder="password"
               fullWidth
+              name='password'
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
             <Grid item xs={12} sm={6}>
-              <Button variant='contained' fullWidth >
+              <Button type='submit' variant='contained' fullWidth >
                 Login
               </Button>
             </Grid>
