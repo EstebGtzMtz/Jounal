b4ddabba7c3c0e79748ada2c0dc303b46d412876
EstebGtzMtz <esteb.gtz.mtz@hotmail.com>
2023-10-05 11:53:10 -0600

    add custom hook to validate logged user

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,28 +1,15 @@
-import { onAuthStateChanged } from "firebase/auth"
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { Navigate, Route, Routes } from "react-router-dom"
-import { AuthRoutes } from "../auth/Routes/AuthRoutes"
-import { FirebaseAuth } from "../firebase/config"
-import { JournalRoutes } from "../journal/Routes/JournalRoutes"
-import { login, logout } from "../store/auth"
-import { CheckingAuth } from "../ui/components"
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { AuthRoutes } from '../auth/Routes/AuthRoutes';
+import { useCheckAuth } from '../hooks';
+import { JournalRoutes } from '../journal/Routes/JournalRoutes';
+import { CheckingAuth } from '../ui/components';
 
 const AppRouter = () => {
 
-	const {status} = useSelector(state => state.auth);
-	const dispatch = useDispatch()
-
-	useEffect(() => {
-		onAuthStateChanged(FirebaseAuth, async user => {
-			if(!user) return dispatch(logout());
-			const {uid, email, displayName, photoURL} = user;
-			dispatch(login({uid, email, displayName, photoURL}));
-		});
-	}, [])
+	const {status} = useCheckAuth();
 
 	if(status === 'checking')
-		return <CheckingAuth />
+		return <CheckingAuth />;
 
   return (
     <Routes>
