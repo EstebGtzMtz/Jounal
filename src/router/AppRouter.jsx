b174f4e75dec2087cbcc7bf189d42867f616b1cb
EstebGtzMtz <esteb.gtz.mtz@hotmail.com>
2023-10-05 11:33:55 -0600

    create loader component and add some route protection to just render the correct pages according to the user status

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,15 +1,38 @@
-import { Route, Routes } from "react-router-dom"
+import { onAuthStateChanged } from "firebase/auth"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Navigate, Route, Routes } from "react-router-dom"
 import { AuthRoutes } from "../auth/Routes/AuthRoutes"
+import { FirebaseAuth } from "../firebase/config"
 import { JournalRoutes } from "../journal/Routes/JournalRoutes"
+import { login, logout } from "../store/auth"
+import { CheckingAuth } from "../ui/components"
 
 const AppRouter = () => {
+
+	const {status} = useSelector(state => state.auth);
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		onAuthStateChanged(FirebaseAuth, async user => {
+			if(!user) return dispatch(logout());
+			const {uid, email, displayName, photoURL} = user;
+			dispatch(login({uid, email, displayName, photoURL}));
+		});
+	}, [])
+
+	if(status === 'checking')
+		return <CheckingAuth />
+
   return (
     <Routes>
-			{/* Login */}
-			<Route path='/auth/*' element={<AuthRoutes />} />
+			{
+				status=== 'authenticated' ?
+				<Route path='/*' element={<JournalRoutes />} /> :
+				<Route path='/auth/*' element={<AuthRoutes />} />
+			}
 
-			{/* Journal App  */}
-			<Route path='/*' element={<JournalRoutes />} />
+			<Route path='/*' element={<Navigate to='/auth/login' />} />
 		</Routes>
   )
 }
--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  status: 'not-authenticated', // not-authenticated, authenticated
+  status: 'not-authenticated', // not-authenticated, authenticated. checking
   uid: null,
   email: null,
   displayName: null,
