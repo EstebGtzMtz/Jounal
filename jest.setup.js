181b674c2642c1862b14d1c5341525d05deae397
EstebGtzMtz <esteb.gtz.mtz@hotmail.com>
2023-11-26 15:22:58 -0600

    add env and resolve error on process.env to jest configuration and the import.meta.env vite configuration

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,10 @@
+/* eslint-disable no-undef */
 import 'whatwg-fetch';
+
+require('dotenv').config({
+	path:'.env.test'
+});
+
+jest.mock('./src/helpers/getEnvironments.js', () => ({
+	getEnvironments: () => ({...process.env})
+}));
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,28 +1,26 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
+import {getEnvironments} from "../helpers/index.js";
 
-//const firebaseConfig = {
-  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  //apiKey: 'AIzaSyCEFEGE_7s6rSWW4VWF_GEUwMKCPUOOCNY',
-  //authDomain: "react-curso-28959.firebaseapp.com",
-  //projectId: "react-curso-28959",
-  //storageBucket: "react-curso-28959.appspot.com",
-  // messagingSenderId: `${import.meta.env.VITE_FIREBASE_API_MESSAGIN_SENDER_ID}`,
-  //messagingSenderId: '28525836881',
-  // appId: `${import.meta.env.VITE_FIREBASE_API_ID}`
-  //appId: '1:28525836881:web:5ae1ce558a33915da4d670'
-//};
+const {
+  VITE_FIREBASE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_FIREBASE_API_MESSAGING_SENDER_ID,
+  VITE_FIREBASE_API_ID
+} = getEnvironments();
 
-console.log(import.meta.env)
 const firebaseConfig = {
-  apiKey: "AIzaSyAgn4Hc9-bOLnlwfQVGuqU9f8YtoabGmes",
-  authDomain: "easy-tickets-9d40c.firebaseapp.com",
-  projectId: "easy-tickets-9d40c",
-  storageBucket: "easy-tickets-9d40c.appspot.com",
-  messagingSenderId: "1055712863538",
-  appId: "1:1055712863538:web:81441fc3f8af0b3aa8232b"
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_API_MESSAGING_SENDER_ID,
+  appId:VITE_FIREBASE_API_ID
 };
+
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
